c0b0520868a9ceb032c2e50c08c9d6c4220d9584
bejitono <stdemicheli@protonmail.com>
2020-09-07 17:18:25 +0700

    Implements list tasks command.

---
+++ list.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Lists all of your tasks",
+	Run: func(cmd *cobra.Command, args []string) {
+		tasks, err := AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You have not tasks to complete! Why not take a vacation?")
+			return
+		}
+		fmt.Println("You have the following tasks:")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(listCmd)
+
+}
